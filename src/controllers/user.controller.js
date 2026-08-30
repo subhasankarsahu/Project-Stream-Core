31c2cecd01234e3005a47a70b036061e594b5ef3
Subha Sankar Sahu <subhasankarsahu5@gmail.com>
2026-08-31 00:51:46 +0530

    Add subpipelines

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { User } from '../models/user.model.js';
 import { uploadOnCloudinary } from '../utils/cloudinary.js';
 import { ApiResponse } from '../utils/ApiResponse.js';
 import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
 
 const generateAccessAndRefreshTokens = async(userId) => {
     try {
@@ -328,7 +329,6 @@ const updateUserCoverImage = asyncHandler(async(req, res) => {
     new ApiResponse(200, user, "Cover Image Updated Successfully")
   )
 })
-
 const getUserChannelProfile = asyncHandler(async(req, res) => {
   const {username} = req.params
 
@@ -396,6 +396,32 @@ const getUserChannelProfile = asyncHandler(async(req, res) => {
   return res
   .status(200)
   .json(new ApiResponse(200, channel[0], "Channel profile fetched successfully"))
+})
+
+const getWatchHistory = asyncHandler(async(req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id)
+      }
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: {
+          $lookup: {
+            from: "users",
+            localField: "owner",
+            foreignField: "_id",
+            as: "owner"
+          }
+        }
+      }
+    }
+  ])
 })
 
 export {
